a7799596d77fbb5aaf76cc922eec00e72ada9d65
Patricio <wearetamo@gmail.com>
2019-07-08 21:22:25 -0400

    adding cors

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const EXPRESS = require('express');
 const FEED_ROUTES = require('./routes/feed');
+const BODY_PARSER = require('body-parser');
 const APP = EXPRESS();
 
+APP.use(BODY_PARSER.json());
+APP.use(BODY_PARSER.urlencoded({ extended: false }));
+APP.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  next();
+});
 APP.use('/feed', FEED_ROUTES);
 
 APP.listen(8080);
