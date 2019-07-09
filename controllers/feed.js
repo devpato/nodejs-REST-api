92041600548d6ac365604c3d6db0b39fb309c89a
Patricio <wearetamo@gmail.com>
2019-07-08 21:23:13 -0400

    created posts controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,26 @@
 exports.getPosts = (req, res, next) => {
-  res
-    .status(200)
-    .json({ posts: [{ title: 'test', content: 'This is a post ' }] });
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'test',
+        content: 'This is a post ',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Pato'
+        },
+        date: new Date()
+      }
+    ]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  console.log(title);
+  res.status(201).json({
+    message: 'Post created succesfully!',
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
 };
