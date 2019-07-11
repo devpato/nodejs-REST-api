bbc961217e314fa13124e92eaa6db42425c2a62d
Patricio <wearetamo@gmail.com>
2019-07-11 17:37:05 -0400

    fixed auth route issue

--- routes/auth.js
+++ routes/auth.js
@@ -4,26 +4,31 @@ const { body } = require('express-validator');
 const User = require('../models/user');
 const AUTH_CONTROLLER = require('../controllers/auth');
 
-ROUTER.put('/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Please enter a valid email')
-    .custom((value, { req }) => {
-      return User.findOne({ email: value }).then(userDoc => {
-        if (userDoc) {
-          return Promise.reject('E-mail address already exists!');
-        }
-      });
-    })
-    .normalizeEmail(),
-  body('password')
-    .trim()
-    .isLength({ min: 5 }),
-  body('name')
-    .trim()
-    .not()
-    .isEmpty()
-]),
-  AUTH_CONTROLLER.signup;
+ROUTER.put(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-Mail address already exists!');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  AUTH_CONTROLLER.signup
+);
+
+//ROUTER.post('/login', AUTH_CONTROLLER.login);
 
 module.exports = ROUTER;
