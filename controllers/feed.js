c7fd488c008e66cfd68bccb4afc6ed47ca26c450
Patricio <wearetamo@gmail.com>
2019-07-10 21:58:20 -0400

    update post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const POST = require('../models/posts');
+const fs = require('fs');
+const path = require('path');
 
 exports.getPosts = (req, res, next) => {
   POST.find()
@@ -81,4 +83,64 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const POST_ID = req.params.postId;
+  const TITLE = req.body.title;
+  const CONTENT = req.body.content;
+  let IMAGE_URL = req.body.image;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const ERROR = new Error('Validation Failed! entered data incorrectly');
+    ERROR.statusCode = 422;
+    throw ERROR;
+  }
+
+  if (req.file) {
+    IMAGE_URL = req.file.path;
+  }
+
+  if (!IMAGE_URL) {
+    const ERROR = new Error('No file picked.');
+    ERROR.statusCode = 422;
+    throw ERROR;
+  }
+  console.log('POST ID', POST_ID);
+  POST.findById(POST_ID)
+    .then(post => {
+      if (!post) {
+        const ERROR = new Error('Post not found');
+        ERROR.statusCode = 404;
+        throw ERROR;
+      }
+
+      if (IMAGE_URL !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = TITLE;
+      post.imageUrl = IMAGE_URL;
+      post.content = CONTENT;
+
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Post updated',
+        post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = FILE_PATH => {
+  FILE_PATH = path.join(__dirname, '..', FILE_PATH);
+  fs.unlink(FILE_PATH, err => console.log(err));
 };
