357e04891a08b5cc57fb39d3aef6c7c342405d8d
Patricio <wearetamo@gmail.com>
2019-07-11 20:37:06 -0400

    addle is auth middleware

--- routes/feed.js
+++ routes/feed.js
@@ -2,13 +2,14 @@ const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 const FEED_CONTROLLER = require('../controllers/feed');
 const { body } = require('express-validator');
-
+const isAuth = require('../middleware/is-auth');
 //GET feed/posts
 ROUTER.get('/posts', FEED_CONTROLLER.getPosts);
 
 //POST feed/post
 ROUTER.post(
   '/post',
+  isAuth,
   [
     body('title')
       .trim()
@@ -24,6 +25,7 @@ ROUTER.get('/post/:postId', FEED_CONTROLLER.getPost);
 
 ROUTER.put(
   '/post/:postId',
+  isAuth,
   [
     body('title')
       .trim()
@@ -35,6 +37,6 @@ ROUTER.put(
   FEED_CONTROLLER.updatePost
 );
 
-ROUTER.delete('/post/:postId', FEED_CONTROLLER.deletePost);
+ROUTER.delete('/post/:postId', isAuth, FEED_CONTROLLER.deletePost);
 
 module.exports = ROUTER;
